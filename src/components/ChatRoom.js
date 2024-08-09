5550ed29503e204584b1efa76ade032474732941
khushal-13 <khushaljogi7@gmail.com>
2024-08-09 22:38:30 +0530

    Commit

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -18,7 +18,7 @@ const ChatRoom = () => {
     }, [userData]);
 
     const connect =()=>{
-        let Sock = new SockJS('http://localhost:8080/ws');
+        let Sock = new SockJS('http://13.232.43.181:8080/ws');
         stompClient = over(Sock);
         stompClient.connect({},onConnected, onError);
     }
